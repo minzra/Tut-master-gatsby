459cb1c6a5662bb02515c632c4c6362270e3152b
minzra <zaraispahany1@gmail.com>
2021-03-14 00:02:19 +1100

    created layout and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from './Nav';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import Nav from '../components/Nav';
 
 export default function Homepage() {
   return (
-    <div>
+    <Layout>
       <Nav />
       <p>Hey! I'm the Homepage</p>
-    </div>
+    </Layout>
   );
 }
